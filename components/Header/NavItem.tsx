6b484991c8a97495b0bbbf15a573d17b612f9ff5
Henrique Nunes <79033028+riquen@users.noreply.github.com>
2024-08-05 19:14:34 -0300

    fix trigger menu

--- components/Header/NavItem.tsx
+++ components/Header/NavItem.tsx
@@ -2,6 +2,8 @@ import { ElementType } from 'react'
 
 import Link from 'next/link'
 
+import { CollapsibleTrigger } from '@/components/ui/collapsible'
+
 type NavItemProps = {
   href: string
   icon: ElementType
@@ -9,8 +11,10 @@ type NavItemProps = {
 }
 
 export const NavItem = ({ href, icon: Icon, title }: NavItemProps) => (
-  <Link href={href} className="flex items-center gap-3 px-3 py-2 rounded-lg active:bg-sky-100">
-    <Icon className="w-5 h-5 stroke-slate-950" />
-    <span className="font-medium text-neutral-700 font-open-sans">{title}</span>
-  </Link>
+  <CollapsibleTrigger asChild>
+    <Link href={href} className="flex items-center gap-3 px-3 py-2 rounded-lg active:bg-sky-100">
+      <Icon className="w-5 h-5 stroke-slate-950" />
+      <span className="font-medium text-neutral-700 font-open-sans">{title}</span>
+    </Link>
+  </CollapsibleTrigger>
 )
